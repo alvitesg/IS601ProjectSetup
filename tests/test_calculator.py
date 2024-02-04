72b8a22008e5eb954dbcf802b8a413e9ca212bce
alvitesg <ga329@njit.edu>
2024-02-03 22:52:45 -0500

    added subtraction language to test calc

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,4 +1,11 @@
-from calculator import add
+'''My calculator test'''
+from calculator import add, substract
 
 def test_addition():
+    '''Test that addition function works'''
     assert add(2,2) == 4
+
+def test_subtraction():
+    '''Test that subtraction function works'''
+    assert substract(2,2) == 0
+    
